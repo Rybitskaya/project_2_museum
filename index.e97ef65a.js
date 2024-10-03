7df07ea49f662bd18977221dca5eba915bed3109
Rybytska <miss.anastasi98@gmail.com>
2024-10-03 23:59:00 +0300

    make build

---
+++ index.e97ef65a.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".follow__form"),t=document.querySelectorAll(".follow__email");e.addEventListener("submit",function(e){e.preventDefault(),t.forEach(function(e){return e.value=""}),textarea.value=""});
+//# sourceMappingURL=index.e97ef65a.js.map
